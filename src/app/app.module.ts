3cb74a95864b21e2620d4e99c603199bfb1baa5f
teragt <teragt2@gmail.com>
2017-09-20 11:03:17 +0900

    ルーティングのとこまで

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroService }  from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(NgModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(NgModel)]
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+       path: '',
+       redirectTo: '/dashboard',
+       pathMatch: 'full' 
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-    bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
